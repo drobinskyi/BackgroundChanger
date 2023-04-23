bd9d271f47797e10b04c102839336be6e8b26af1
Vitalii Drobinskyi <drobinskyi@hotmail.com>
2023-04-23 20:06:18 +0300

    Created function to change colors

---
+++ js/app.js
@@ -0,0 +1,14 @@
+const button = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+const colors = ["#FF5733", "#F9FF33", "#3371FF", "red", "green", "rgb(85, 255, 51)"];
+
+button.addEventListener("click", () => {
+    let hexColor = colors[getRandomNumber()];
+    document.body.style.backgroundColor = hexColor;
+    color.textContent = hexColor;
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * colors.length)
+};
